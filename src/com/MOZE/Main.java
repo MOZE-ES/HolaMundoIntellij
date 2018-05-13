0e6a17a4cbd292e37534dd38a85e16b4fc54ed01
Mindaugas Ozeraitis <mindo@hotmail.es>
2018-05-13 18:23:01 +0200

    Versión inicial

--- src/com/MOZE/Main.java
+++ src/com/MOZE/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("La calificación de " + args[0] + " : ");
         System.out.println(veredicto(args[1],args[2]));
-
     }
+    
     public static String veredicto(String valor1, String valor2){
         if (valor1.equals(valor2)) {
             return "Perfecto. Has tenido buen criterio.";
